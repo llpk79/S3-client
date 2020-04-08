5e1e452085943343cd6bc76bc898e54257965aa8
Paulus Kutrich <pkutrich@gmail.com>
2020-04-08 14:21:40 -0700

    Fix login view

--- file_share/auth.py
+++ file_share/auth.py
@@ -1,25 +1,26 @@
-import functools
-
-from flask import Blueprint, g, redirect, flash, render_template, request, url_for, session, abort
-from werkzeug.security import check_password_hash, generate_password_hash
+from flask import Blueprint, g, redirect, flash, render_template, request, url_for, session
+from werkzeug.security import generate_password_hash
 from file_share import db_session
 from flask_login import login_manager
 from .models import User
 from urllib.parse import urlparse, urljoin
-from flask_security import login_user, LoginForm, RegisterForm
+from flask_security import login_user, RegisterForm, logout_user
+from .forms import NewLoginForm
 from sqlalchemy.sql import select
 from datetime import datetime
+
 auth = Blueprint('auth', __name__, url_prefix='/auth')
 
 
 @auth.before_app_request
 def load_logged_in_user():
     user_id = session.get('user_id')
+    print('logged in user ', user_id)
 
     if user_id is None:
         g.user = None
     else:
-        g.user = User.query.filter(User.id == user_id)
+        g.user = User.query.filter_by(id=user_id).one()
 
 
 login_manager.user_loader = load_logged_in_user
@@ -32,36 +33,27 @@ def is_safe_url(target):
 
 
 @auth.route('/login', methods=['GET', 'POST'])
-def login():
+def mylogin():
     # Here we use a class of some kind to represent and validate our
     # client-side form data. For example, WTForms is a library that will
     # handle this for us, and we use a custom LoginForm to validate.
-    form = LoginForm()
-    print('method ', request.method)
-    if request.method == 'POST':
-        if form.validate_on_submit():
-            # Login and validate the user.
-            # user should be an instance of your `User` class
-            email_ = form.email.data
-            password = form.password.data
-            print('form data ', email_, password)
-            # s = select([User.id]).where(User.email == email_)
-            # user = db_session.execute(s).fetchone()
-            # user = User.query.filter(User.email == email_).one()
-            # print('user ', user)
-            # if check_password_hash(user.password, password):
-            #     login_user(user)
-            #
-            # flash('Logged in successfully.')
-            #
-            # next_ = request.args.get('next')
-            # # is_safe_url should check if the url is safe for redirects.
-            # # See http://flask.pocoo.org/snippets/62/ for an example.
-            # if not is_safe_url(next_):
-            #     return abort(400)
-            #
-            # return redirect(next or url_for('index'))
-        return render_template('security/login_user.html', login_user_form=form)
+    form = NewLoginForm()
+    if form.validate_on_submit():
+        # Login and validate the user.
+        # user should be an instance of your `User` class
+        user = User.query.filter_by(email=form.email.data).one()
+        g.user = user
+        session['user_id'] = user.id
+        login_user(user, remember=form.remember.data)
+        flash('Logged in successfully.')
+
+        next_ = request.args.get('next')
+        print('next ', next_)
+
+        return redirect(next_) if next_ else redirect(url_for('routes.index'))
+    else:
+        print('no validation')
+    return render_template('security/login_user.html', login_user_form=form)
 
 
 @auth.route('/register', methods=('GET', 'POST'))
@@ -76,18 +68,24 @@ def register():
             stmt = select([User.id]).where(User.email == email_)
             result = db_session.execute(stmt).fetchone()
 
-            print('result ', result)
+            print('result ', result, password)
 
             if result:
                 error = 'User {} is already registered.'.format(email_)
 
             if error is None:
                 user = User(email=email_, username=email_, password=generate_password_hash(password),
-                            current_login_at=str(datetime.now()))
+                            current_login_at=str(datetime.now()), active=True)
                 db_session.add(user)
                 db_session.commit()
-                return redirect(url_for('auth.login'))
+                return redirect(url_for('auth.mylogin'))
 
             flash(error)
-
     return render_template('security/register_user.html', register_user_form=form)
+
+
+@auth.route('/logout')
+def logout():
+    logout_user()
+    session.clear()
+    return redirect(url_for('routes.index'))
