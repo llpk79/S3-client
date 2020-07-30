bc8b44cdcfd44d4634ea83ec49bab4f958d8c55e
llpk79 <paul.kutrich@slalom.com>
2020-07-29 23:23:04 -0700

    Clean up.

--- application/s3_functions.py
+++ application/s3_functions.py
@@ -1,14 +1,12 @@
-"""Functions for interacting with S3 buckets."""
+"""Functions for interacting with AWS S3."""
 import boto3
 
 
 def upload_file(file_name: str, bucket_name: str) -> None:
     """Uploads <file_name> to S3 bucket <bucket_name>."""
-    object_name = file_name
     s3 = boto3.client("s3")
-
     try:
-        s3.upload_file(file_name, bucket_name, object_name)
+        s3.upload_file(file_name, bucket_name, file_name)
     except Exception as e:
         print(e)
 
@@ -16,25 +14,22 @@ def upload_file(file_name: str, bucket_name: str) -> None:
 def download_file(file_name: str, bucket_name: str) -> bytes:
     """Downloads <file_name> from S3 bucket <bucket_name>."""
     s3 = boto3.client("s3")
-
     try:
         file = s3.get_object(Bucket=bucket_name, Key=file_name)["Body"].read()
     except Exception as e:
         print(e)
         return b""
-
     return file
 
 
-def list_files(bucket_name: str) -> list:
+def get_file_list(bucket_name: str) -> list:
     """Lists files in S3 bucket <bucket_name>."""
     s3 = boto3.client("s3")
     files = []
-
     try:
         for item in s3.list_objects(Bucket=bucket_name)["Contents"]:
             files.append(item)
-    except KeyError:
+    except KeyError:  # Bucket is empty and has no key `Contents`.
         pass
 
     return files
