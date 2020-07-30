16dadb8379418d9ce2d3208e4021067adbdcd4fb
llpk79 <paul.kutrich@slalom.com>
2020-07-29 20:52:52 -0700

    Comment out `with open...` from download.

--- application/routes.py
+++ application/routes.py
@@ -77,11 +77,11 @@ def download(file_name):
         db_session.begin()
         file.last_download = f"{datetime.now()}"
         db_session.commit()
-        with open(output, "w"):
-            return (
-                send_file(filename_or_fp=output, as_attachment=True),
-                delete_file(file_name),
-            )
+        # with open(output, "w"):
+        return (
+            send_file(filename_or_fp=output, as_attachment=True),
+            delete_file(file_name),
+        )
 
 
 def delete_file(file_name):
